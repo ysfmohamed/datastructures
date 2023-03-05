fe4097d5019143eb8c0ea8cc5004bb9e15a6b687
Youssef Mohamed <yussf.mohamed.o@gmail.com>
2023-03-05 15:21:21 +0200

    add explanation comment to the rotate right function

--- AVL Tree/Tree/src/main/java/org/example/AVLTree.java
+++ AVL Tree/Tree/src/main/java/org/example/AVLTree.java
@@ -129,6 +129,78 @@ public class AVLTree
     private TreeNode rotateRight(TreeNode nodeToBeBalanced) {
         // IMPORTANT NOTE: imbalanced node must have left child
 
+        /*
+            Let's take an example, assume that we have a balanced binary search tree like that:
+                        60
+                     57    70
+                   55   58
+
+            and we want to insert(50).
+
+            the balanced binary search tree becomes unbalanced as follows:
+                        60
+                     57    70
+                   55   58
+                 50
+
+            Let's calculate balance factor of every node after inserting(58):
+            bf of node(50) = 0 - 0 = 0
+            bf of node(70) = 0 - 0 = 0
+            bf of node(58) = 0 - 0 = 0
+            bf of node(55) = 1 - 0 = 1
+            bf of node(57) = 2 - 1 = 1
+            bf of node(60) = 3 - 1 = 2
+
+            so node(60) becomes imbalanced, and we must balance it to convert the unbalanced bst to balanced bst.
+
+            Solution:
+            - since bf of node(60) is 2 (LEFT HEAVY) and bf of node(57) is 1, so it means that we made a Left-Left imbalance insertion, so
+              we should do Right Rotation.
+
+            Right Rotation:
+                1- pass the imbalanced node to the rotateRight method, and lets call it "x".
+
+                2- assign the left child of x to a temporary variable called "lc".
+                   TreeNode leftChildOfImbalancedNode = nodeToBeBalanced.getLeft();
+
+                3- since we are doing right rotation, so we must handle the parents of x and lc.
+                   so we need to assign the parent of x in the parent of lc,
+                   and we need to assign lc in the parent of x
+                   leftChildOfImbalancedNode.setParent(nodeToBeBalanced.getParent());
+                   nodeToBeBalanced.setParent(leftChildOfImbalancedNode);
+
+                   in the parents context, lc becomes the father of x and x becomes the child of lc.
+
+                3- now our lc node has two children one on the left and the other on the right, and we must handle that right child,
+                   to handle it just unplug it from the lc and plug it into the x as a left child.
+                   nodeToBeBalanced.setLeft(leftChildOfImbalancedNode.getRight());
+
+                   and its parent becomes x, so we must also handle this case
+                   if(leftChildOfImbalancedNode.getRight() != null)
+                       leftChildOfImbalancedNode.getRight().setParent(nodeToBeBalanced);
+
+                4- now, move down the x to the right, and then move the lc up to take the place of x,
+                   and plug the x as right child of lc.
+                   leftChildOfImbalancedNode.setRight(nodeToBeBalanced);
+
+            so the tree becomes:
+                         57
+                        /  \
+                      55    60
+                     /     /  \
+                   50     58   70
+
+            Let's calculate balance factor of every node after balancing:
+            bf of node(50) = 0 - 0 = 0
+            bf of node(70) = 0 - 0 = 0
+            bf of node(58) = 0 - 0 = 0
+            bf of node(55) = 1 - 0 = 1
+            bf of node(57) = 2 - 2 = 0
+            bf of node(60) = 1 - 1 = 0
+
+            now the binary search tree becomes balanced.
+        * */
+
         TreeNode leftChildOfImbalancedNode = nodeToBeBalanced.getLeft();
         leftChildOfImbalancedNode.setParent(nodeToBeBalanced.getParent());
         nodeToBeBalanced.setParent(leftChildOfImbalancedNode);
